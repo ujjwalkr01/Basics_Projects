c49ac3f03add605ca5641316ec08836e0f6c43b7
ujjwlkr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-05-06 11:04:47 +0530

    commit

--- ModalWindow/index.js
+++ ModalWindow/index.js
@@ -2,7 +2,7 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnClose = document.querySelector(".close-modal");
 const btnOpen = document.querySelectorAll(".show-modal");
-console.log(btnOpen);
+// console.log(btnOpen);
 
 const OpenModal = function () {
   // console.log('Button clicked');
@@ -22,3 +22,19 @@ for (let i = 0; i < btnOpen.length; i++) {
 //here we are not calling the function because java script will execute this function when it will come at line 20, 23, or  24 so we want that when we click then only the action to be performed so we just declare the function.
 btnClose.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+
+//we can also use keyboard property to perfrom functionality...
+/*There are three keyboard property
+ 1. keydown--> performs action when key is pressed.
+ 2. keyup--> performs action when key is lift.
+ 3. keypress--> perfroms action when key is continously pressed.
+
+ Note--> If we want to view which key is pressed then just pass the parameter in the function and just use parameter.key inside the console.log
+*/
+document.addEventListener('keydown', function(e){
+  console.log(e.key);
+  if(e.key=='Escape' && !modal.classList.contains('hidden')){
+      closeModal();
+  }
+})
